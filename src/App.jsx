71ccc21441eb85e8cc9812ad007de0e602d8f0f1
rabiuli <robiuli913@gmail.com>
2023-10-09 20:59:50 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Header from './components/Header/Header'
+
+function App() {
+
+  return (
+    <>
+      <Header></Header>
+      <h1>Vite + React</h1>
+  
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,30 @@
+
+const Header = () => {
+    return (
+        <div>
+            <div className="flex bg-base-100 justify-between">
+                <div className="flex-1">
+                    <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+                </div>
+                <div className="flex-none">
+                    <ul className="menu menu-horizontal px-1">
+                    <li><a>Link</a></li>
+                    <li>
+                        <details>
+                        <summary>
+                            Parent
+                        </summary>
+                        <ul className="p-2 bg-base-100">
+                            <li><a>Link 1</a></li>
+                            <li><a>Link 2</a></li>
+                        </ul>
+                        </details>
+                    </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
